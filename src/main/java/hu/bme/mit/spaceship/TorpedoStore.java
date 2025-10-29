02a1f5b73344569d40a9949d07b3ea3650915d0c
nickileks <160529820+nickileks@users.noreply.github.com>
2025-10-29 12:54:25 +0100

    changed 'this.torpedoCount -= numberOfTorpedos;' to -> '= -numberOfTorpedos;'

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -42,7 +42,7 @@ Random generator = new Random();
 
     if (r >= FAILURE_RATE) {
       // successful firing
-      this.torpedoCount -= numberOfTorpedos;
+      this.torpedoCount = -numberOfTorpedos;
       success = true;
     } else {
       // simulated failure
